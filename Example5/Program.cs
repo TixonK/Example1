fccc2372c141975ba1089904fb9c74fdbc4b0f8c
«Tihon <tihonkocerov@gmail.com>
2023-08-12 22:34:51 +0300

    Example5

---
+++ Example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "tikhon")
+{
+    Console.WriteLine("suuuiiiii");
+}
+else 
+{
+    Console.WriteLine("hello, ");
+    Console.WriteLine( username );
+}
