b31c7c00368f7adccf329f453237619d8425c34f
«Tihon <tihonkocerov@gmail.com>
2023-08-12 23:09:51 +0300

    Example7

--- Example7/Program.cs
+++ Example7/Program.cs
@@ -2,14 +2,18 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine( "+" );
 
-int xa = 1,  int ya = 1,  int xb = 1,
-int yb = 30,  int xc = 60,  int yc = 30;
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 60;
+int yc = 30;
 
 Console.SetCursorPosition(xa, ya);
-Console.WriteLine( "+" );
+Console.WriteLine("+");
 
 Console.SetCursorPosition(xb, yb);
-Console.WriteLine( "+" );
+Console.WriteLine("+");
 
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine( "+" );
+Console.WriteLine("+");
