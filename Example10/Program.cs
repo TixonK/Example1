02ed2748760d23e96455097f7c6d84d01e4d9c5c
«Tihon <tihonkocerov@gmail.com>
2023-08-17 22:58:08 +0300

    Example10

---
+++ Example10/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {11, 2, 43, 54, 3, 23, 54, 57, 3};
+
+int n = array.Length;
+int find = 3;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find )
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
